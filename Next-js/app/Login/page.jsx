5566bc3d7e6490b328b73ae60b8f2e9b74f9bbd0
maulana adam <wandgenius6@gmail.com>
2024-03-26 09:01:41 +0700

    update app

--- Next-js/app/Login/page.jsx
+++ Next-js/app/Login/page.jsx
@@ -4,29 +4,21 @@ import "./style.css";
 
 const LoginPage = () => {
 	return (
-      <main>
-         <div className="wrapper">
-            <form action="">
-               <h1>Login</h1>
-               <div className="input-box">
-                  <p>Username</p>
-                  <input type="text" />
-                  <i className="bx bxs-user"></i>
-               </div>
-               <div className="input-box">
-                  <p>Password</p>
-                  <input type="password" />
-                  <i className="bx bxs-user"></i>
-               </div>
-               <p>Belum punya akun?</p>
-               <br />
-               <div className="btn-field">
-                  <button type="button">Login</button>
-                  <button type="button">Daftar</button>
-               </div>
-            </form>
-         </div>
-      </main>
+		<main>
+			<div class="wrapper">
+				<h1>Login</h1>
+				<form action="#">
+					<input type="text" placeholder="Username" />
+					<input type="password" placeholder="Password" />
+					<div class="akun">Belum punya akun?</div>
+					<br />
+					<div class="btn-field">
+						<button type="button">Login</button>
+						<button type="button">Daftar</button>
+					</div>
+				</form>
+			</div>
+		</main>
 	);
 };
 
